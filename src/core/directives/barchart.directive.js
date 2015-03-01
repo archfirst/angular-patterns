e55849b4038f3ab4cce4535e63d4f6027fefcd59
nareshbhatia <naresh.archfirst@gmail.com>
2015-03-01 12:36:51 -0500

    Added bar chart.directive.js
    This file got left out of the last commit

---
+++ src/core/directives/barchart.directive.js
@@ -0,0 +1,81 @@
+/*
+    A directive to draw bar charts using D3. The directive itself takes
+    care of the Angular side of the instrumentation, the D3 rendering
+    is handled by the BarChart class provided by the barchart factory.
+
+    Note: the prefix "at" in "at-barchart" stands for "angular-template".
+    You should choose a unique and descriptive prefix for your directives.
+
+    Usage:
+        <at-barchart data-chartdata="vm.chartdata"></at-barchart>
+
+    Chart data should be in the following format:
+
+        [
+            {
+                'key': 'US Stocks',
+                'value': 90,
+            },
+            {
+                'key': 'Cash',
+                'value': 10,
+            },
+            ...
+        ];
+*/
+
+(function () {
+    'use strict';
+
+    angular
+        .module('app.core')
+        .directive('atBarchart', barchartDirective);
+
+    barchartDirective.$inject = ['BarChart', '$window', '_'];
+
+    /* @ngInject */
+    function barchartDirective(BarChart, $window, _) {
+
+        var directive = {
+            link: link,
+            restrict: 'E',
+            scope: {
+                chartdata: '='
+            }
+        };
+        return directive;
+
+        function link(scope, element) {
+            var tableRowHeight = 37; // TODO: take out hard coding
+
+            // initialize the chart
+            var svgElement = element.html('<svg>').children()[0];
+            var barChart = new BarChart(svgElement);
+            barChart.barHeight(tableRowHeight);
+
+            // Redraw whenever chartdata changes
+            scope.$watch('chartdata', drawChart);
+
+            // Redraw whenever window resizes, adding some debounce
+            angular.element($window).on('resize', _.debounce(drawChart, 250));
+
+            // Remove the redraw handler when the scope is destroyed
+            // This prevents redrawing when the view containing the barchart is destroyed
+            scope.$on('$destroy', function() {
+                angular.element($window).off('resize', drawChart);
+            });
+
+            function drawChart() {
+                var chartdata = scope.chartdata;
+
+                // This can happen when the server has not yet returned the chartdata
+                if (!chartdata) { return; }
+
+                barChart
+                    .width(element.width())
+                    .draw(chartdata);
+            }
+        }
+    }
+
+})();
