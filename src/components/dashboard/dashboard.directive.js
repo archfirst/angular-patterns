aac2fe0205cd969ce1614c10e929f7628b1448f1
Pavan Podila <pavan.podila@gmail.com>
2015-03-21 11:15:00 -0400

    example of inlining the DashboardController. For simpler components there is no need to create a separate file for the controller.

--- src/components/dashboard/dashboard.directive.js
+++ src/components/dashboard/dashboard.directive.js
@@ -6,10 +6,43 @@
             return {
                 restrict: 'E',
                 templateUrl: 'components/dashboard/dashboard.html',
-                controller: 'DashboardController',
+                controller: DashboardController,
                 controllerAs: 'vm',
                 bindToController: true
             };
         });
+
+    DashboardController.$inject = ['accountService', 'logger', '_'];
+    function DashboardController(accountService, logger, _) {
+        var vm = this;
+
+        vm.account = null;
+        vm.chartdata = null;
+
+        activate();
+
+        function activate() {
+            return getAccount().then(function() {
+                logger.info('Activated Dashboard View');
+            });
+        }
+
+        function getAccount() {
+            return accountService.getAccount().then(function(data) {
+
+                // Convert assets to chart data
+                var chartdata = _.map(data.assets, function(asset) {
+                    return {
+                        key: asset.asset_class,
+                        value: asset.percent_allocation * 100
+                    };
+                });
+
+                vm.account = data;
+                vm.chartdata = chartdata;
+                return vm.account;
+            });
+        }
+    }
 
 })();
