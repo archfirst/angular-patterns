916ef71e66e53716a67edd668ad167235336c2de
nareshbhatia <naresh.archfirst@gmail.com>
2015-03-22 12:03:50 -0400

    Removed bindToController property of shared scope directives
    This property makes sense for only isolate scope directives

--- src/components/profile/profile.directive.js
+++ src/components/profile/profile.directive.js
@@ -3,8 +3,7 @@
     'use strict';
 
     angular.module('app.profile')
-        .directive('profile', profileDirective)
-        .controller('ProfileController', ProfileController);
+        .directive('profile', profileDirective);
 
 
     // ----- profileDirective -----
@@ -15,30 +14,10 @@
 
         var directive = {
             restrict: 'E',
-            templateUrl: 'components/profile/profile.html',
-            controller: 'ProfileController',
-            controllerAs: 'vm',
-            bindToController: true
+            templateUrl: 'components/profile/profile.html'
         };
 
         return directive;
-    }
-
-
-    // ----- ProfileController -----
-    ProfileController.$inject = ['logger'];
-
-    /* @ngInject */
-    function ProfileController(logger) {
-        var vm = this;
-
-        vm.user = {};
-
-        activate();
-
-        function activate() {
-            logger.info('Activated Profile View');
-        }
     }
 
 })();
